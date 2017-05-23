3ff2032be355a93f82d22f8cc4d4481d5adaf579
whatbeg <huqiu00@163.com>
2017-05-23 16:55:12 +0800

    add feature_engineering module

---
+++ src/feature_engineering.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+
+def binary_search(val, array, start=0):
+    """
+    binary search implementation
+
+    :param val: value to search
+    :param array: data array to be searched
+    :param start: 0 if array starts with 0 else 1
+    :return: location of val in array, or bucket fall in if not in array
+    """
+    low = start
+    high = len(array) - 1 + start
+    while low <= high:
+        mid = (low + high) / 2
+        if array[mid] == val:
+            return mid
+        elif array[mid] > val:
+            high = mid-1
+        else:
+            low = mid+1
+    return low
+
+
+def bucketized_column(column, boundaries):
+    """
+    transform every value of a column to corresponding bucket according to boundaries
+
+    :param column: primitive column
+    :param boundaries: boundaries to bucketize
+    :return: bucketized column
+    """
+    import copy
+    _column = copy.deepcopy(column)
+    for i in range(len(_column)):
+        _column[i] = binary_search(_column[i], boundaries)
+    return _column
---
+++ test/feature_engineeringSpec.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+import src.feature_engineering as feng
+
+
+class feature_engineeringSpec():
+
+    def __init__(self):
+        pass
+
+    def binarySearchSpec(self):
+
+        array = [18, 25, 30, 35, 40, 45, 50, 55, 60, 65]
+        assert feng.binary_search(7, array) == 0
+        # print ("feng.binary_search(7, array) == {}".format(feng.binary_search(7, array)))
+        assert feng.binary_search(20, array) == 1
+        assert feng.binary_search(80, array) == 10
+        assert feng.binary_search(-1, array) == 0
+
+    def bucketized_columnSpec(self):
+
+        column = [5, 29, 30, 43, 64, 89]
+        boundaries = [18, 25, 30, 35, 40, 45, 50, 55, 60, 65]
+        feature_column = feng.bucketized_column(column, boundaries)
+        # print (feature_column)
+        assert feature_column == [0, 2, 2, 5, 9, 10]
+
+    def doTest(self):
+        self.binarySearchSpec()
+        self.bucketized_columnSpec()
+        print ("All Test Passed!")
+
+if __name__ == '__main__':
+    feSpec = feature_engineeringSpec()
+    feSpec.doTest()
+
